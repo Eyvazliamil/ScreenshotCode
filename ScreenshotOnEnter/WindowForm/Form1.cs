5cec3912b6885029a2e9e8368db50fe32327dc6b
Eyvazliamil <167743489+Eyvazliamil@users.noreply.github.com>
2026-07-05 00:06:55 +0400

    Update Form1.cs

--- ScreenshotOnEnter/WindowForm/Form1.cs
+++ ScreenshotOnEnter/WindowForm/Form1.cs
@@ -1,46 +1,21 @@
-﻿using ScreenshotOnEnter.TakeScreenShot;
+using ScreenshotOnEnter.TakeScreenShot;
 using System.Diagnostics;
 
 namespace ScreenshotOnEnter.WindowForm
 {
     public partial class Form1 : Form
     {
-        private readonly string folderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Images");
-
+        private readonly string folderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Images"); 
         public Form1()
         {
             InitializeComponent();
 
             if (!Directory.Exists(folderPath))
-                Directory.CreateDirectory(folderPath);
-
-            //KeyPreview = true;
-            //KeyDown += Form1_KeyDown;
+                Directory.CreateDirectory(folderPath); 
 
             listBox1.DoubleClick += listBox1_DoubleClick;
-        }
-
-        // ENTER basılanda Screenshot çək
-        //private void Form1_KeyDown(object sender, KeyEventArgs e)
-        //{  
-        //    if (e.KeyCode == Keys.Enter)
-        //    {
-        //        string fileName = $"Screenshot_{DateTime.Now:yyyyMMdd_HHmmssfff}.png";
-        //        string fullPath = Path.Combine(folderPath, fileName);
-
-        //        TakeAScreenshot.Capture(fullPath);
-
-        //        MessageBox.Show("Screenshot saved.");
-        //    }
-        //}
-
-        // Şəkillərin adlarını ListBox-a yüklə
-        //private void button1_Click(object sender, EventArgs e)
-        //{
-        //    GetFiles(folderPath);
-        //}
-
-        // Çıxış
+        } 
+         
         private void button2_Click(object sender, EventArgs e)
         {
             Application.Exit();
@@ -57,9 +32,8 @@ namespace ScreenshotOnEnter.WindowForm
                 listBox1.Items.Add(file);
                 Console.WriteLine(Path.GetFileName(file));
             }
-        }
-
-        // Şəkli Default Program (Photos) ilə aç
+        } 
+        
         private void listBox1_DoubleClick(object sender, EventArgs e)
         {
             if (listBox1.SelectedItem != null)
@@ -70,11 +44,6 @@ namespace ScreenshotOnEnter.WindowForm
                     UseShellExecute = true
                 });
             }
-        }
-
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
         }
 
         protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
@@ -88,13 +57,17 @@ namespace ScreenshotOnEnter.WindowForm
                 TakeAScreenshot.Capture(fullPath);
 
                 MessageBox.Show("Screenshot saved: " + fullPath);
-                return true; // Enter-in düymələrə ötürülməsinin qarşısını alır
+                return true;  
             }
 
             return base.ProcessCmdKey(ref msg, keyData);
         }
 
         private void label1_Click(object sender, EventArgs e)
+        {
+
+        } 
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
